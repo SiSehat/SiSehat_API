34ed68ed93e4fc739d5e6fff516e2cb1c85e8471
dhino12 <dhinorahmad0@gmail.com>
2022-11-16 01:58:47 +0700

    add POST obat

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-import admin from 'firebase-admin'
+import { initializeApp } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
 import * as config from './config.js';
 import serviceAccount from './sisehat-api-4c78d-firebase-adminsdk-698pu-8c65ba3ce1.json' assert { type: 'json' }
 
-admin.initializeApp({
+initializeApp({
     credential: admin.credential.cert(serviceAccount)
 })
 
-const db = admin.firestore();
+const db = getFirestore();
 
 export default db;
