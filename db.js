7dadb3144cef30274a5a30db342a869c5a95300f
dhino12 <dhinorahmad0@gmail.com>
2022-11-16 23:00:18 +0700

    test

--- db.js
+++ db.js
@@ -5,6 +5,13 @@ import config from './config.js';
 // const firestore = require('@firebase/firestore');
 // const config = require('./config');
 
-const db = firebase.initializeApp(config.firebaseConfig)
+const db = firebase.initializeApp({
+    apiKey: "AIzaSyAM76apksRb45hAOdBYo1ViSdCCABKTQnE",
+    authDomain: "sisehat-api-4c78d.firebaseapp.com",
+    projectId: "sisehat-api-4c78d",
+    storageBucket: "sisehat-api-4c78d.appspot.com",
+    messagingSenderId: "482710330133",
+    appId: "1:482710330133:web:3070f173cba2178459e71c"
+})
 
 export default db;
