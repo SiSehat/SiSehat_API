579d8944548ae9da604238fe544bf0055fa174dc
dhino12 <dhinorahmad0@gmail.com>
2022-11-16 20:56:13 +0700

    test

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const { initializeApp } = require('firebase/app');
+const { initializeApp } = require('@firebase/app');
 const firestore = require('@firebase/firestore');
 const config = require('./config');
 
