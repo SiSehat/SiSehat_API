553a965c1ac965931024dff38065b789258db29f
dhino12 <dhinorahmad0@gmail.com>
2022-11-16 20:50:15 +0700

    fix POST data to firestore

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const { initializeApp } = require('firebase/app');
-const { getFirestore } = require('firebase/firestore');
-const firestore = require('firebase/firestore');
+const firestore = require('@firebase/firestore');
 const config = require('./config');
-
-// import { initializeApp } from 'firebase/app'
-// import { getFirestore } from 'firebase/firestore'
-// import * as config from './config.js';
-// import serviceAccount from './sisehat-api-4c78d-firebase-adminsdk-698pu-8c65ba3ce1.json' assert { type: 'json' }
 
 initializeApp(config.firebaseConfig)
 
