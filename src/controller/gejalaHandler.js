55719417f7cf6d5dea1b41f86aa254f84840f903
dhino12 <dhinorahmad0@gmail.com>
2022-11-25 14:53:16 +0700

    search symptom example

--- src/controller/gejalaHandler.js
+++ src/controller/gejalaHandler.js
@@ -5,24 +5,51 @@ let getCollection = null;
 
 const sympthomHandler = async (request, h) => {
     const symptoms = request.payload.symptom;
+    let response = null
 
     getCollection = firestore.collection('disease')
-    console.log(symptoms);
-    let data = await getCollection
-        .where('symptom', "in", [symptoms]).get();
-        
-    let result = [];
-    data.forEach(user => {
-        console.log(user.exists);
-        result.push(user.data())
-    })
 
-    const response = h.response({
-        status: "success",
-        data: result
-    })
+    try {
+        let data = await getCollection
+            .where('symptom', "array-contains-any", symptoms).get()
 
-    response.code(200);
+        let result = [];
+        data.docs.forEach((user) => {
+            result.push(user.data())
+        })
+
+        let countVerif = [];
+        let tmp = [];
+        let dataSymptom = [];
+        let realData = [];
+        result.forEach((symptomsData, i) => {
+            dataSymptom = [];
+            symptoms.forEach((symptom, i) => {
+                tmp = symptomsData.symptom.filter(data => data === symptom)
+                if (tmp.length > 0) {
+                    dataSymptom.push(...tmp)
+                }
+            })
+            countVerif.push(dataSymptom)
+            if (i > 0 && countVerif[i - 1].length < countVerif[i].length) {
+                realData = symptomsData;
+            }
+            console.log('ganti data');
+        })
+
+        console.log(realData);
+        // console.log(realData);
+
+        response = h.response({
+            status: "success",
+            data: realData
+        })
+    
+        response.code(200);
+    } catch (error) {
+        console.log(error);
+    }
+
     return response
 }
 
